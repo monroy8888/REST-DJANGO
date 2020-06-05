32a05fa80685db13f2944f26f1be8ea4be20bbc1
vagrant <vagrant@vagrant.vm>
2020-06-05 04:45:52 +0000

    change env

--- djapi/api/admin.py
+++ djapi/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import *
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(SubCategoria)
+admin.site.register(Producto)
+admin.site.register(Clientes)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
--- djapi/api/models.py
+++ djapi/api/models.py
@@ -43,3 +43,19 @@ class Producto(models.Model):
 
     class Meta:
         verbose_name_plural = "Productos"
+
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField(blank=True, null=True)
+    tfno=models.CharField(max_length=7)
+
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=50)
+    precio=models.IntegerField()
+
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateTimeField()
+    entregado=models.BooleanField()
